79e4b69b36eb3fa8d003892a3c5317c3b67a3395
ushie <ekunkeushie@gmail.com>
2024-05-09 10:10:36 +0100

    added auth to the form

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import { useLogin } from "./useLogin";
 import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-  const [email, setEmail] = useState("ushie@example.com");
-  const [password, setPassword] = useState("games12345");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const { login, isLoading } = useLogin();
 
   function handleSubmit(e) {
